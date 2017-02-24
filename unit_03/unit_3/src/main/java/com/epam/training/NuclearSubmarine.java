e74ae7e4968c1bdd55cc99450bf27a4b36db4798
Ilya Kulakov <ilya.kulakov.4308@gmail.com>
2017-02-24 17:37:12 +0300

    Task 6. Inner Class added.

--- unit_03/unit_3/src/main/java/com/epam/training/NuclearSubmarine.java
+++ unit_03/unit_3/src/main/java/com/epam/training/NuclearSubmarine.java
@@ -4,4 +4,54 @@ package com.epam.training;
  * Created by Ilya Kulakov on 24.02.17.
  */
 public class NuclearSubmarine {
+    private String vesselName;
+    private int numberOfTorpedo;
+    private double nuclearFuel = 10.;
+
+    public NuclearSubmarine() {
+    }
+
+    public NuclearSubmarine(String Name, int numberOfTorpedo) {
+        setVesselName(Name);
+        setNumberOfTorpedo(numberOfTorpedo);
+    }
+
+    public String getVesselName() {
+        return vesselName;
+    }
+
+    public void setVesselName(String vesselName) {
+        this.vesselName = vesselName;
+    }
+
+    public int getNumberOfTorpedo() {
+        return numberOfTorpedo;
+    }
+
+    public void setNumberOfTorpedo(int numberOfTorpedo) {
+        this.numberOfTorpedo = numberOfTorpedo;
+    }
+
+    public void fight(int shots) {
+        for (int i = 0; i <= shots; i++) {
+            if (this.getNumberOfTorpedo() <= 0){
+                System.out.println("Can`t fight without weapons");
+            return;
+        }
+            System.out.println("Piu-Piu");
+            setNumberOfTorpedo(getNumberOfTorpedo() - 1);
+        }
+    }
+
+    public void swimmDistance(int distance){
+        class NuclearEngine {
+            public void startEngene(){
+                nuclearFuel = nuclearFuel - distance * 0.01;
+                System.out.println(nuclearFuel);
+            }
+        }
+        NuclearEngine engine = new NuclearEngine();
+        engine.startEngene();
+
+    }
 }
--- unit_03/unit_3/src/test/java/com/epam/training/NuclearSubmarineTest.java
+++ unit_03/unit_3/src/test/java/com/epam/training/NuclearSubmarineTest.java
@@ -1,10 +1,18 @@
 package com.epam.training;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by Ilya Kulakov on 24.02.17.
  */
 public class NuclearSubmarineTest {
+
+   @Test
+    public void submarineEngineTest() {
+       NuclearSubmarine submarine = new NuclearSubmarine("fish", 10);
+        submarine.swimmDistance(5);
+   }
 
 }
