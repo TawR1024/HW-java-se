e4f9ab2a62ee7d6b088d70eec4de6e9495e86c0c
Ilya Kulakov <ilya.kulakov.4308@gmail.com>
2017-02-24 15:16:43 +0300

    NuclearSubmarine class added

---
+++ unit_03/unit_3/src/main/java/com/epam/training/NuclearSubmarine.java
@@ -0,0 +1,7 @@
+package com.epam.training;
+
+/**
+ * Created by Ilya Kulakov on 24.02.17.
+ */
+public class NuclearSubmarine {
+}
---
+++ unit_03/unit_3/src/test/java/com/epam/training/NuclearSubmarineTest.java
@@ -0,0 +1,10 @@
+package com.epam.training;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Ilya Kulakov on 24.02.17.
+ */
+public class NuclearSubmarineTest {
+
+}
