0f3227f9ede2e4925b661390f003622ee710cfc9
Ilya Kulakov <ilya.kulakov.4308@gmail.com>
2017-02-26 13:44:25 +0300

    getLastMessage method added

--- unit_03_3/unit_3_3/src/main/java/com/epam/training/CrazyLogger.java
+++ unit_03_3/unit_3_3/src/main/java/com/epam/training/CrazyLogger.java
@@ -5,14 +5,20 @@ package com.epam.training;
  */
 public class CrazyLogger {
     StringBuilder log = new StringBuilder();
+    int indexOfLastMessageExclusiveFirst = 0;
 
     public void addNewLogMessage(String message) {
         log.append(message);
         log.append(";\n");
+        log.trimToSize();
+        indexOfLastMessageExclusiveFirst = log.capacity()-message.length()-2;
     }
 
-    public String getLastMessage() {
-        return log.toString();
+    public String getLastMessage()throws RuntimeException {
+        log.trimToSize();
+        if (log.capacity() == 0)
+            throw new RuntimeException("CrazyLogger is empty");
+        return log.substring(indexOfLastMessageExclusiveFirst);
     }
     public void showLog() {
         System.out.println(log);
--- unit_03_3/unit_3_3/src/test/java/com/epam/training/CrazyLoggerTest.java
+++ unit_03_3/unit_3_3/src/test/java/com/epam/training/CrazyLoggerTest.java
@@ -22,11 +22,50 @@ public class CrazyLoggerTest {
     @Test
     public void showLogTest() {
         LocalDateTime now = LocalDateTime.now();
-        String message =  now.format(DateTimeFormatter.ofPattern("dd-mm-YYYY:hh-mm")) + " - Error #1";
+        String message1 =  now.format(DateTimeFormatter.ofPattern("dd-mm-YYYY:hh-mm")) + " - Error #1";
+        String message2 =  now.format(DateTimeFormatter.ofPattern("dd-mm-YYYY:hh-mm")) + " - Error #2";
+        String message3 =  now.format(DateTimeFormatter.ofPattern("dd-mm-YYYY:hh-mm")) + " - Error #3";
+        String message4 =  now.format(DateTimeFormatter.ofPattern("dd-mm-YYYY:hh-mm")) + " - Error #4";
+        String message5 =  now.format(DateTimeFormatter.ofPattern("dd-mm-YYYY:hh-mm")) + " - Error #5";
+        String message6 =  now.format(DateTimeFormatter.ofPattern("dd-mm-YYYY:hh-mm")) + " - Error #6";
+        String message7 =  now.format(DateTimeFormatter.ofPattern("dd-mm-YYYY:hh-mm")) + " - Error #7";
         CrazyLogger logger = new CrazyLogger();
 
-        logger.addNewLogMessage(message);
+        logger.addNewLogMessage(message1);
+        logger.addNewLogMessage(message2);
+        logger.addNewLogMessage(message3);
+        logger.addNewLogMessage(message4);
+        logger.addNewLogMessage(message5);
+        logger.addNewLogMessage(message6);
+        logger.addNewLogMessage(message7);
         logger.showLog();
+    }
+
+    @Test
+    public void getLastMessageTest() {
+        CrazyLogger logger = new CrazyLogger();
+        LocalDateTime now = LocalDateTime.now();
+        String message1 =  now.format(DateTimeFormatter.ofPattern("dd-mm-YYYY:hh-mm")) + " - Error #1";
+        String message7 =  now.format(DateTimeFormatter.ofPattern("dd-mm-YYYY:hh-mm")) + " - Compile Error #7";
+
+        logger.addNewLogMessage(message1);
+        logger.addNewLogMessage(message7);
+
+        assertTrue(logger.getLastMessage().contains("7"));
+    }
+
+    @Test
+    public void getLastMessageTestIfLoggerIsEmpty() {
+        CrazyLogger logger = new CrazyLogger();
+
+        try {
+            logger.getLastMessage();
+        }catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            logger.addNewLogMessage(e.getMessage());
+            assertTrue(logger.getLastMessage().contains(e.getMessage()));
+        }
+
     }
 
 }
