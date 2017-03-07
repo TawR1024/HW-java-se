1b515c703fee1f77a64cdc23d845ad55092b41a8
Ilya Kulakov <ilya.kulakov.4308@gmail.com>
2017-03-07 12:37:32 +0300

    makor changes

--- unit_4/unit_4/src/test/java/com/epam/training/task4/FilmCollectionTest.java
+++ unit_4/unit_4/src/test/java/com/epam/training/task4/FilmCollectionTest.java
@@ -1,6 +1,9 @@
 package com.epam.training.task4;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Collections;
 
 import static org.junit.Assert.*;
 
@@ -8,15 +11,26 @@ import static org.junit.Assert.*;
  * Created by Ilya Kulakov on 06.03.17.
  */
 public class FilmCollectionTest {
+
+    FilmCollection filmCollection;
+    Actor actor1;
+    Actor actor2;
+    Actor actor3;
+    Film film3;
+
+    @Before
+    public void init() {
+        filmCollection = new FilmCollection("MyCollection");
+        actor1 = new Actor("Bob", 15);
+        actor2 = new Actor("Jack", 89);
+        actor3 = new Actor("John", 45);
+        film3 = new Film("Kin-dza-dza", "genre 2");
+    }
+
     @Test
     public void saveCollectionAsFile() throws Exception {
-        Actor actor1 = new Actor("Bob", 15);
-        Actor actor2 = new Actor("Jack", 89);
-        Actor actor3 = new Actor("John", 45);
         Film film1 = new Film("Film Name 1", "comedy");
         Film film2 = new Film("Film name 2", "genre 1");
-        Film film3 = new Film("Kin-dza-dza", "genre 2");
-        FilmCollection filmCollection = new FilmCollection("MyCollection");
         filmCollection.addFilm(film1, actor1);
         filmCollection.addFilm(film2, actor2);
         filmCollection.addFilm(film3, actor3);
@@ -24,15 +38,20 @@ public class FilmCollectionTest {
     }
 
     @Test
+    public void AddToCollectionOneFilmWithThreeActors() {
+        filmCollection.addFilm(film3, actor1, actor2, actor3);
+    }
+
+
+    @Test
     public void addToCollection() {
-        FilmCollection filmCollection = new FilmCollection("MyCollection");
         Actor actor1 = new Actor("Bob", 15);
         Film film = new Film("Kin-dza-dza", "comedy");
+        filmCollection.addFilm(film, actor1);
     }
 
     @Test(expected = NullPointerException.class)
     public void addEmptyArguments() {
-        FilmCollection filmCollection = new FilmCollection();
         Actor actor = null;
         Film film = null;
         filmCollection.addFilm(film, actor);
