34d148e6f5d2c3bd9ddb1881e3eea22f863ece19
Ilya Kulakov <ilya.kulakov.4308@gmail.com>
2017-03-10 15:23:53 +0300

    Start solving task2

--- unit_4/unit_4/src/main/java/com/epam/training/task1/ByteIOStreamss.java
+++ unit_4/unit_4/src/main/java/com/epam/training/task1/ByteIOStreamss.java
@@ -59,7 +59,6 @@ public class ByteIOStreamss {
         }
     }
 
-
     /**
      * Analyse file and count keywords.
      */
--- unit_4/unit_4/src/main/java/com/epam/training/task2/SymbolStream.java
+++ unit_4/unit_4/src/main/java/com/epam/training/task2/SymbolStream.java
@@ -1,9 +1,25 @@
 package com.epam.training.task2;
 
+import com.epam.training.task1.ByteIOStreamss;
+
 /**
  * Created by Ilya Kulakov on 07.03.17.
  */
-public class SymbolStream {
+public class SymbolStream extends ByteIOStreamss {
 
+    @Override
+    public void readFile() {
+
+    }
+
+    @Override
+    public void analyseJavaFile() {
+
+    }
+
+    @Override
+    public void writeResultToFile(String path) {
+
+    }
 
 }
