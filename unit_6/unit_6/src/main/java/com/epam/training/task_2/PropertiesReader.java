636c8914ff0a6bc80c9bddc57f3cec2924a3d5e7
Ilya Kulakov <ilya.kulakov.4308@gmail.com>
2017-03-26 22:05:47 +0300

    *readPropertyFile() +readPropertiesIntoMap()

--- unit_6/unit_6/src/main/java/com/epam/training/task_2/PropertiesReader.java
+++ unit_6/unit_6/src/main/java/com/epam/training/task_2/PropertiesReader.java
@@ -1,6 +1,12 @@
 package com.epam.training.task_2;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Properties;
 
 /**
  * Created by Ilya Kulakov on 26.03.17.
@@ -12,9 +18,39 @@ public class PropertiesReader {
         this.propeties = file;
     }
 
-    public void readPropertyFile() {
+    /**
+     * @param path
+     * @throws IOException
+     */
+    public void readPropertyFile(Path path)throws  IOException {
+        Objects.requireNonNull(path);
+
+        Properties properties = new Properties();
+
+        try(FileInputStream fileInputStream = new FileInputStream(path.toFile())) {
+            properties.load(fileInputStream);
+        }
+        Map<String, String> data = readPropertiesIntoMap(properties);
     }
 
-    public void getProperty() {
+    /**
+     *
+     * Read properties and convert data from file to Map.
+     * @param properties
+     * @return <code>dataFromProperties</code>converted HashMap from properies to Map
+     */
+    private Map<String,String> readPropertiesIntoMap(Properties properties) {
+        Map<String, String> dataFromProperties = new HashMap<>();
+
+        for (Map.Entry<Object, Object> pair : properties.entrySet()){
+            String key = (String) pair.getKey();
+            String value = (String) pair.getValue();
+
+            dataFromProperties.put(key, value);
+        }
+        return dataFromProperties;
+    }
+
+    public void getProperty(String key) {
     }
 }
